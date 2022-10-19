d17cfbcbf6d3d6f653a174454569ac19b49661e3
Arthur Mayer <arthur.mayer@cap4lab.com>
2022-10-19 13:34:38 +0200

    add models

--- src/billy/models.py
+++ src/billy/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Summary(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, null=True)
+    year = models.IntegerField(default=0)
+    month = models.IntegerField(default=0)
+    sold = models.FloatField(default=0)
+
+
+class Movement(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, null=True)
+    year = models.IntegerField(default=0)
+    month = models.IntegerField(default=0)
+    day = models.IntegerField(default=0)
+    lib = models.CharField(max_length=200)
+    value = models.FloatField(default=0)
+    sold = models.FloatField(default=0)
+    rec = models.BooleanField()
